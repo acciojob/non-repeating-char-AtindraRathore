a394dd433fa3114976c47fe16bbba7c946a63c42
Atindra Singh Rathore <118996145+AtindraRathore@users.noreply.github.com>
2023-12-13 18:55:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(let i=0;i<str.length-2;i+=2){
+		if(str[i] !== str[i+1] && str[i+1] === str[i+2]){
+			return str[i];
+		}
+	}
+	return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
